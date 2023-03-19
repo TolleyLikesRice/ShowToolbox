363281c91e91355059dcd4a7f337b5836ffefdd2
TolleyLikesRice <tolley@tolley.tech>
2023-03-19 13:52:14 +0000

    Inital

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Show_Toolbox"
+include(":androidApp")
+include(":shared")
